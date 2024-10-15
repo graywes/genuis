efc2378b9c0a70e8226511b87f95e15e9bba7913
graywes <wdgrayson@outlook.com>
2024-10-14 23:40:29 -0700

    final

--- genuis.py
+++ genuis.py
@@ -1,16 +1,29 @@
+import multiprocessing
+from random import randint, randrange
+import random
 import time
 import winsound as s
-from mido import MidiFile
-from beep import beep
+from mido import bpm2tempo, tick2second, MidiFile
 import chippy
-import pyaudio
 from pathlib import Path
+#import threading
+import simpleaudio as sa
+import wave
+from pydub import AudioSegment
+from pydub.playback import _play_with_simpleaudio
+from pydub.playback import play as audi
+import keyboard
 import asyncio
-import threading
+import msvcrt as m
+from multiprocessing import Process, freeze_support, get_context, set_start_method, get_all_start_methods
+import os
 
-mid = MidiFile('Pirates of the Caribbean - Hes a Pirate.mid')
-synth = chippy.Synthesizer(framerate=44100)
+
+mid = MidiFile('Tarrega_Adelita.mid')
+synth = chippy.Synthesizer(framerate=48000)
 square_pcm = synth.pulse_riff(length=3, frequency=183, duty_cycle=25)
+
+
 
 #for i in range(300):
 #    print("""\
@@ -21,17 +34,19 @@ square_pcm = synth.pulse_riff(length=3, frequency=183, duty_cycle=25)
 
 frame = 0
 
+note = {}
+play = {}
 
-player = ["Hero", 100, 10]
-enemy = ["Freddy Fazbear", 100, 10] # (enemy_name, health, attack)
+player = ["Hero", int(open('diff.txt').readlines()[2]), 10]
+enemy = ["Freddy Fazbear", int(open('diff.txt').readlines()[1]), int(open('diff.txt').readlines()[0])] # (enemy_name, health, attack)
 art = open('fail.txt').read() #current enemy ascii art
 redraw = open('blank.txt').read()
 bear_attack = enemy[2]
 hero_attack = player[2]
 block = enemy[2] - 5
+done = False
 
-
-
+"""
 class Music(threading.Thread):
 
     def __init__(self, function_that_downloads):
@@ -40,18 +55,28 @@ class Music(threading.Thread):
 
     def run(self):
         self.runnable()
-
+"""
 
 def music():
+    time.sleep(2.3)
     for msg in mid.play():
         if msg.type == 'note_on':
+            mus = note[msg.note]
+            play[msg.note] = _play_with_simpleaudio(mus + ((msg.velocity - 64) * 0.5))
             #s.Beep(int((400 / 32) * (2 ** ((msg.note - 9) / 12))), 500)
-            s.PlaySound(("wavefile" + str(msg.note) + ".wav"), s.SND_ASYNC)
-        elif msg.type == 'note_off':
-            s.PlaySound(None, s.SND_ALIAS)
-
+            #s.PlaySound(("wavefile" + str(msg.note) + ".wav"), s.SND_ASYNC)
+            #note = sa.WaveObject.from_wave_file(("wavefile" + str(msg.note) + ".wav"))
+            #audio_data = note[msg.note].readframes(note[msg.note].getnframes())
+            #num_channels = note[msg.note].getnchannels()
+            #bytes_per_sample = note[msg.note].getsampwidth()
+            #sample_rate = note[msg.note].getframerate()
+            #play[msg.note] = sa.play_buffer(audio_data, num_channels, bytes_per_sample, sample_rate)
+        #elif msg.type == 'note_off':
+            #s.PlaySound(None, s.SND_ALIAS)
+            play[msg.note].stop()
 
 def draw():
+    global done
     global frame
     global art
     print(redraw)
@@ -72,23 +97,50 @@ def draw():
         print(art)
         
 
+def dam(dam, defe=0):
+    print("you took " + str(dam) + " amount of damage")
+    if defe:
+        print("you negated " + str(defe))
+    global player
+    
+    player[1] -= dam
+
 def Hero():
     print(redraw)
     print(art)
     print("a wild " + enemy[0] + " attacks!")
-    print("enemy hp: " + str(enemy[1]))
-    print("Hero: " + "HP " + str(player[1]) + " | " + "AP " + str(player[2]))
+    print("enemy hp: " + str(enemy[1]) + " | dmg: " + str(enemy[2]))
+    print("Hero: " + "HP " + str(player[1]))
 
 def ask():
     print("")
+    seep = 1.3
     print("CHOOSE YOUR ACT!")
-    print("attack (1), defend (2), or item (3)")
-    ask = input("ACT: ")
-    if ask == "1" or ask == "attack":
+    print("attack (1), or defend (2)?")
+    m.getch()
+    roll = random.randint(0, 4)
+    #print(roll)
+    if roll == 1:
+        player[2] = random.randint(9, 16)
+        for i in range(10):
+            print("it's a crit!!")
+        seep = 1.5
+    else:
+        player[2] = randint(8, 10)
+    if keyboard.is_pressed('1'):
         enemy[1] -= player[2]
+        s.PlaySound('Explosion6', s.SND_NODEFAULT)
+        print("you attack with " + str(player[2]))
+        dam(enemy[2])
+        time.sleep(seep)
+    if keyboard.is_pressed('2'):
+        defe = random.randint(0, 4)
+        dam(enemy[2] - defe, defe)
+        time.sleep(seep)
 
 art = open('bear.txt').read()
-s.Beep(800, 100)
+
+
 for i, track in enumerate(mid.tracks):
     print('Track {}: {}'.format(i, track.name))
     for msg in track:
@@ -96,12 +148,54 @@ for i, track in enumerate(mid.tracks):
             if not Path(("wavefile" + str(msg.note) + ".wav")).is_file():
                 square_pcm = synth.pulse_riff(length=0.5, frequency=int((400 / 32) * (2 ** ((msg.note - 9) / 12))), duty_cycle=25)
                 synth.save_wave(square_pcm, ("wavefile" + str(msg.note) + ".wav"))
- 
-draw()
+            note[msg.note] = AudioSegment.from_wav(("wavefile" + str(msg.note) + ".wav"), 'rb')
 art = open('bear.txt').read()
+print(get_all_start_methods())
+#done = True
+if __name__ == '__main__':
+    freeze_support()
+    global thread
+    thread = Process(target=music)
+    thread.start()
+    print("wajfsiuhjfhbnrsdiujhghbrfnug hjfdnbg ujsrf hgusrf ghry8u  gt")
+draw()
 Hero()
-thread = Music(music)
-thread.start()
-ask()
-Hero()
-ask()
+while enemy[1] >= 0:
+    Hero()
+    ask()
+    if player[1] <= 0:
+        thread.terminate()
+        mid = MidiFile('marche.mid')
+        note = {}
+        play = {}
+        for i, track in enumerate(mid.tracks):
+            print('Track {}: {}'.format(i, track.name))
+            for msg in track:
+                if msg.type == 'note_on':
+                    if not Path(("wavefile" + str(msg.note) + ".wav")).is_file():
+                       square_pcm = synth.pulse_riff(length=0.5, frequency=int((400 / 32) * (2 ** ((msg.note - 9) / 12))), duty_cycle=25)
+                       synth.save_wave(square_pcm, ("wavefile" + str(msg.note) + ".wav"))
+                    note[msg.note] = AudioSegment.from_wav(("wavefile" + str(msg.note) + ".wav"), 'rb')
+        for i in range(100):
+            print("you lose!!!!")
+        music()
+        exit(None)
+        
+print("you winnn, yay")
+for i in range(100):
+    print("you winnn, yay")
+
+thread.terminate()
+mid = MidiFile('Tetris - B Theme.mid')
+note = {}
+play = {}
+for i, track in enumerate(mid.tracks):
+    print('Track {}: {}'.format(i, track.name))
+    for msg in track:
+        if msg.type == 'note_on':
+            if not Path(("wavefile" + str(msg.note) + ".wav")).is_file():
+                square_pcm = synth.pulse_riff(length=0.5, frequency=int((400 / 32) * (2 ** ((msg.note - 9) / 12))), duty_cycle=25)
+                synth.save_wave(square_pcm, ("wavefile" + str(msg.note) + ".wav"))
+            note[msg.note] = AudioSegment.from_wav(("wavefile" + str(msg.note) + ".wav"), 'rb')
+
+music()
